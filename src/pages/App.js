8abb08e566f42e4eb21bf1a1b7d6414180967451
Artem <moranmr8@gmail.com>
2019-10-18 21:16:41 +0300

    added my gifts

--- src/pages/App.js
+++ src/pages/App.js
@@ -12,7 +12,7 @@ import Spinner from "../components/shared/Spinner";
 import Header from "../components/Header"
 import Main from "./Main";
 import Friends from "./Friends";
-
+import MyGifts from "./My"
 import profile from "../data/profile";
 import friends from "../data/friends";
 
@@ -25,6 +25,7 @@ class App extends React.Component {
                 <Friends />
                 <hr/>
                 <Header profile={profile} friends={friends} />
+                <hr/>
                 <Title>Wishlist &#128525;</Title>
                 <hr/>
                 <Avatar img={profile.img}/>
@@ -46,6 +47,8 @@ class App extends React.Component {
                 <Spinner />
                 <hr/>
                 <SearchBar/>
+                <hr/>
+                <MyGifts />
             </div>
         );
     }
---
+++ src/pages/My/MyGifts.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Header from "../../components/Header/Header"
+import Avatar from "../../components/shared/Avatar"
+import Title from "../../components/shared/Title"
+import Button from "../../components/shared/Button";
+import ItemBox from "../../components/shared/ItemBox"
+import profile from "../../data/profile";
+import friends from "../../data/friends";
+import "./MyGifts.css"
+import WishListItems from "../../data/WishListItems";
+import Masonry from "react-masonry-component";
+import Item from "../../components/shared/ItemBox/ItemBox";
+
+class MyGifts extends React.Component {
+
+
+
+    render() {
+        return (
+            <div className="myGifts">
+                <Header profile={profile} friends={friends} />
+                <div className="myGifts__info">
+                    <Avatar img={profile.img} size={150}/>
+
+                    <div className="right">
+                        <Title children="Ким хуим"/>
+                        <div className="info__link-row">
+                            <a href="123" className="active">Хочу получить</a>
+                            <a href="123" className="">Хочу подарить</a>
+                        </div>
+                        <Button width={125} children="Поделиться"/>
+
+                    </div>
+
+                </div>
+                <ItemBox items={WishListItems} />
+
+            </div>
+
+        )
+    }
+}
+
+export default MyGifts;
