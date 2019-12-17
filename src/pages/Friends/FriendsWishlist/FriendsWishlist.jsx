b1bd4af257c1c5eff975d1652ec68140f37ddf4d
Verrok <kimverrok@gmail.com>
2019-12-18 00:37:06 +0300

    вывод вишлиста друга

--- src/pages/Friends/FriendsWishlist/FriendsWishlist.jsx
+++ src/pages/Friends/FriendsWishlist/FriendsWishlist.jsx
@@ -6,6 +6,8 @@ import Title from 'components/shared/Title';
 
 import './FriendsWishlist.css';
 
+
+import api from 'api';
 import ItemBox from '../../Main/ItemBox';
 import MainContext from '../../MainContext';
 
@@ -23,13 +25,41 @@ class FriendsWishlist extends React.Component {
     this.setState({
       profile,
     });
+    this.getItems();
   }
+
+  getItems = () => {
+    // eslint-disable-next-line react/prop-types
+    const { profile } = this.state;
+    // eslint-disable-next-line no-const-assign
+    api('/api/user/friends', 'POST', {
+      ids: [profile.id],
+    }).then((result) => {
+      console.log(result.response);
+      // eslint-disable-next-line no-underscore-dangle
+      const friendId = result.response.friends[1]._id;
+      api('/api/wishlist/get', 'GET', {
+        id: friendId,
+      }).then((res) => {
+        if (res.response) {
+          this.setState({
+            items: res.response.wishlist,
+          });
+        } else {
+          console.error(res);
+        }
+        return null;
+      }).catch((e) => console.error(e));
+
+      return true;
+    }).catch((e) => console.error(e));
+  };
 
   static contextType = MainContext;
 
   render() {
     const { items, profile } = this.state;
-    console.log(profile);
+    const { wishlistIds } = this.context;
     return (
       <div className="myGifts">
         <div className="myGifts__info">
@@ -39,7 +69,7 @@ class FriendsWishlist extends React.Component {
           </div>
         </div>
         <div className="myGifts__box">
-          <ItemBox items={items} />
+          <ItemBox items={items} compareTo={wishlistIds} />
         </div>
 
       </div>
